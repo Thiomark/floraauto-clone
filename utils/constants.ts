8615df5c7810a4f9545e14c95ff0e77a3c6957f7
itumeleng <thiotsoela@gmail.com>
2022-09-17 17:14:38 +0200

    first commit

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+export const store_name = 'Subesh Auto Services';
+
+export const background_image = 'https://images.pexels.com/photos/3807120/pexels-photo-3807120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+
+export const links = [
+    {name: 'Home', url: '/'},
+    {name: 'About', url: '/about'},
+    {name: 'Services', url: '/services'},
+    {name: 'ContactUs', url: '/contact us'}
+]
